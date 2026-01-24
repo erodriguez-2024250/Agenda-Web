1f4cf4a29e9a369df66fd1ec528cae48f67d66a8
erodriguez-2024250 <erodriguez-2024250@kinal.edu.gt>
2026-01-24 00:51:02 -0600

    add: Agregar login.js en carpeta JS.

---
+++ JS/login.js
@@ -0,0 +1,9 @@
+document.getElementById("loginForm").addEventListener("submit", e => {
+    e.preventDefault();
+
+    const correo = document.getElementById("correo").value;
+
+    localStorage.setItem("correoUsuario", correo);
+
+    window.location.href = "contactos.html";
+});
